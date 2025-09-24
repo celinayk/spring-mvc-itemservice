f6c05ae0cea6d7047b6e24d362ae524cecc1143f
YeonKyung Ryu <celina16161616@gmail.com>
2025-09-24 19:19:44 +0900

    t컨트롤러 생성 및 상품등록처리 v1

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,67 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor //final이 붙은애로 생성자 만들어준다
+public class BasicItemController {
+
+  private final ItemRepository itemRepository;
+
+  @GetMapping
+  public String items(Model model) {
+    List<Item> items = itemRepository.findAll();
+    model.addAttribute("items", items);
+    return "basic/items";
+  }
+
+  @GetMapping("/{itemId}")
+  public String item(@PathVariable Long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/item";
+  }
+
+  @GetMapping("/add")
+  public String addForm() {
+    return "basic/addForm";
+  }
+
+  // 상품 등록 처리
+  @PostMapping("/add")
+  public String addItemV1(@RequestParam String itemName,
+                          @RequestParam int price,
+                          @RequestParam Integer quantity,
+                          Model model) {
+    Item item = new Item();
+    item.setItemName(itemName);
+    item.setPrice(price);
+    item.setQuantity(quantity);
+    itemRepository.save(item);
+    model.addAttribute("item", item);
+    return "basic/item";
+  }
+
+
+
+
+  /**
+   * 테스트용 데이터 추가
+   */
+  @PostConstruct
+  public void init() {
+    itemRepository.save(new Item("testA", 10000, 10));
+    itemRepository.save(new Item("testB", 20000, 20));
+  }
+
+
+}
