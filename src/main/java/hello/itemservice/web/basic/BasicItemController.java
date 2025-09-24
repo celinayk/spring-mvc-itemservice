4806c215442d0a16957c66cbf2207e8a3d01f02f
YeonKyung Ryu <celina16161616@gmail.com>
2025-09-24 19:40:05 +0900

    t상품 수정처리, 상품 등록시 리다이렉트

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -72,7 +72,7 @@ public class BasicItemController {
    * model.addAttribute(item); 자동 추가, 생략 가능
    * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
    */
-  @PostMapping("/add")
+  //@PostMapping("/add")
   public String addItemV3(@ModelAttribute Item item) {
     itemRepository.save(item);
     return "basic/item";
@@ -82,10 +82,33 @@ public class BasicItemController {
    * @ModelAttribute 자체 생략 가능
    * model.addAttribute(item) 자동 추가
    */
-  @PostMapping("/add")
+  //@PostMapping("/add")
   public String addItemV4(Item item) {
     itemRepository.save(item);
     return "basic/item";
+  }
+
+  /**
+   * PRG - Post/Redirect/Get
+   */
+  @PostMapping("/add")
+  public String addItemV5(Item item) {
+    itemRepository.save(item);
+    return "redirect:/basic/items/" + item.getId();
+  }
+
+
+  @GetMapping("/{itemId}/edit")
+  public String editForm(@PathVariable Long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/editForm";
+  }
+
+  @PostMapping("/{itemId}/edit")
+  public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    itemRepository.update(itemId, item);
+    return "redirect:/basic/items/{itemId}";
   }
 
 
